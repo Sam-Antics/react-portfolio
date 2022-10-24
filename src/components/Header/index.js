3437661282f9e441d9c4b0147d7310c0e76f9724
Samantha Guerra <88997930+Sam-Antics@users.noreply.github.com>
2022-10-24 00:14:17 -0500

    started the Header before making the feature branch for it

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,12 +5,25 @@ import {
   Grid,
   Box
 } from '@material-ui/core';
+import Typed from 'react-typed';
 import avatar from "../../assets/images/sam-avatar.png";
 
 const Header = () => {
   return (
     <Box>
       <Avatar src={avatar} alt="Sam's Avatar" />
+      <Typography variant="h4">
+        <Typed strings={["Samantha Guerra"]} typeSpeed={40} />
+      </Typography>
+      <br/>
+      <Typography variant="h5">
+        <Typed 
+        strings={["Web Design", "Web Development", "MERN Stack"]} 
+        typeSpeed={40} 
+        backSpeed={16}
+        loop
+        />
+      </Typography>
     </Box>
   )
 }
