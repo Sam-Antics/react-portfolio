cef5a15f538e50a2be1815ad592d3a9058241408
Samantha Guerra <88997930+Sam-Antics@users.noreply.github.com>
2022-10-26 12:31:51 -0500

    beginning routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import { Route } from 'react-router-dom';
 import './App.css';
 
 // components
 import Home from './components/Home';
+import Resume from './components/Resume';
 
 const App = () => {
   return (
     <>
     <CssBaseline />
-      <Home />
+      <Route exact path="/" component={Home} />
+      <Route path="/resume" component={Resume} />
     </>
   );
 }
